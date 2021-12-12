8187fc4ee69251cfcf8c1d14d091e8231b44c1ba
Anya Bu <annabukhtiiarova@gmail.com>
2021-12-12 14:22:54 +0200

    file structure corrected

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-let string = 'SUCK Lorem FUCK Fuck SuCk ipsum dolor sit amet, fuck consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nisl tincidunt eget nullam non. Quis hendrerit dolor magna eget est lorem ipsum dolor sit. Volutpat odio facilisis mauris sit amet massa. Commodo odio aenean sed adipiscing diam donec adipiscing tristique. Mi eget mauris pharetra et. Non tellus orci ac auctor augue. Elit at imperdiet dui accumsan sit. Ornare arcu dui vivamus arcu felis. Egestas integer eget aliquet nibh praesent. In hac habitasse platea dictumst quisque sagittis purus. Pulvinar elementum integer enim neque volutpat ac.';
+let text = 'SUCK Lorem FUCK Fuck SuCk ipsum dolor sit amet, fuck consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nisl tincidunt eget nullam non. Quis hendrerit dolor magna eget est lorem ipsum dolor sit. Volutpat odio facilisis mauris sit amet massa. Commodo odio aenean sed adipiscing diam donec adipiscing tristique. Mi eget mauris pharetra et. Non tellus orci ac auctor augue. Elit at imperdiet dui accumsan sit. Ornare arcu dui vivamus arcu felis. Egestas integer eget aliquet nibh praesent. In hac habitasse platea dictumst quisque sagittis purus. Pulvinar elementum integer enim neque volutpat ac.';
 
 
-function filter(string) {
-    let result;
+function filterOutBadWords(text) {
+    let arrayWithoutBadWords;
 
-    string = string.split(' ');
+    let textArray = text.split(' ');
 
-    result = string.filter(function(word) { 
+    arrayWithoutBadWords = textArray.filter(function(word) { 
 
-            if (word.toUpperCase() !== "fuck".toUpperCase() && word.toUpperCase() !== "SUCK") {
+            if (word.toUpperCase() !== "fuck".toUpperCase() && word.toUpperCase() !== "suck".toUpperCase()) {
                 return word;
             }
     });
 
-    return result.join(' ');
+    return arrayWithoutBadWords.join(' ');
  
 }
 
-console.log(filter(string));
+console.log(filterOutBadWords(text));
 
 
